c96296af20b1c1e3dac60cdc4bb63a455989b639
Yara Studer <yarastuder@gmail.com>
2025-04-15 15:56:01 -0400

    Added moveable animated player with temp assets

---
+++ Player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+var move_speed : float = 100
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _physics_process(_delta):
+	# Move
+	var input_direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
+	velocity = input_direction * move_speed
+	move_and_slide()
+	pick_new_state()
+	update_animation_parameters(velocity)
+
+func update_animation_parameters(move_input : Vector2):
+	if (move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
