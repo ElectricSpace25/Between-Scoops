e3dc0fb4d26f46413d81d2eebc00f7bf3b1959a8
Yara Studer <yarastuder@gmail.com>
2025-04-15 17:14:11 -0400

    Added modular ice cream

---
+++ IceCream/ice_cream.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var scoop1 = $Scoop1
+@onready var scoop2 = $Scoop2
+@onready var scoop3 = $Scoop3
+
+var scoops = 0
+
+func _ready():
+	add_scoop(3)
+	add_scoop(1)
+	add_scoop(2)
+	add_scoop(3)
+
+func add_scoop(flavor):
+	# 1 = Vanilla, 2 = Chocolate, 3 = Strawberry
+	if (scoops == 0):
+		scoop1.frame = flavor
+	elif (scoops == 1):
+		scoop2.frame = flavor
+	elif (scoops == 2):
+		scoop3.frame = flavor
+	else:
+		print("Max scoops reached!")
+	
+	scoops += 1
