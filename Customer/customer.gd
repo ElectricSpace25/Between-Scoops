9e6f9f438213bcce8d258cf9e9de051a5da3f152
Yara Studer <yarastuder@gmail.com>
2025-04-20 19:26:49 -0400

    Added customer respawning

--- Customer/customer.gd
+++ Customer/customer.gd
@@ -2,22 +2,23 @@ extends Node2D
 
 @onready var ice_cream = $IceCream
 @onready var interactable: Area2D = $Interactable
-@onready var player_ice_cream = $"../IceCream"
+@onready var player_ice_cream = $"../../IceCream"
 
 func _ready():
 	interactable.interact = _on_interact
 
+	# Create random order
 	ice_cream.add_cone()
 	var num_of_scoops = (randi() % 3) + 1
 	for i in range(num_of_scoops):
 		var flavor = (randi() % 3) + 1
 		ice_cream.add_scoop(flavor)
-	print(ice_cream.scoops)
 	
 func _on_interact():
 	if player_ice_cream.hasCone && player_ice_cream.scoops == ice_cream.scoops:
 		print("Correct order!")
 		player_ice_cream.clear()
+		get_parent().spawn_customer()
 		queue_free()
 	else:
 		print("Wrong order!")
--- Levels/Shop/customer_area.gd
+++ Levels/Shop/customer_area.gd
@@ -3,6 +3,12 @@ extends Node2D
 var customer = load("res://Customer/customer.tscn")
 
 func _ready():
+	spawn_customer()
+	
+func spawn_customer():
+	await(get_tree().create_timer((randi() % 3) + 1).timeout)
 	var new_customer = customer.instantiate()
 	add_child(new_customer)
 	new_customer.position = Vector2i(272, 244)
+	
+		
