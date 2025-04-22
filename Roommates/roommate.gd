26274e95a34d9a5ba67c87a00b307794a1f63d45
claudiflower <liharcl@gmail.com>
2025-04-22 16:02:35 -0400

    Initial dialogue system fetching logic

--- Roommates/roommate.gd
+++ Roommates/roommate.gd
@@ -7,11 +7,47 @@ extends CharacterBody2D
 @export var roommate_id : String # use for logic checks and quest/dialogue assignments
 @export var roommate_name : String # displayed on UI when player interacts
 
-func _ready():
-	interactable.interact = _on_interact
+# Dialogue variables
+@export var dialogue_resource : Dialogue # instances Dialogue resource
+var current_state = "start"
+var current_branch_index = 0
 
+func _ready():
+	# Roommates are interactable
+	interactable.interact = _on_interact
+	
+	# Load all dialogue data
+	dialogue_resource.load_from_json("res://Resources/Dialogue/dialogue_data.json")
+
+# When player interacts with Roommate, start the dialogue
 func _on_interact():
+	print("Interacting with " + roommate_name)
 	start_dialogue()
 
+# Get roommate's individual dialogue
 func start_dialogue():
-	print("Hello from Roommate!")
+	# Returns npc id's tree
+	var roommate_dialogues = dialogue_resource.get_npc_dialogue(roommate_id)
+	if roommate_dialogues.is_empty():
+		return
+	get_current_dialogue()
+
+# Get current dialogue based on current state and branch index
+func get_current_dialogue():
+	var roommate_dialogues = dialogue_resource.get_npc_dialogue(roommate_id)
+	if current_branch_index < roommate_dialogues.size():
+		for dialogue in roommate_dialogues[current_branch_index]["dialogues"]:
+			if dialogue["state"] == current_state:
+				print("Roomate says: " + dialogue["text"])
+				return dialogue
+	return null
+
+# Set initial Dialogue branch and state
+func set_dialogue_tree(branch_index):
+	current_branch_index = branch_index
+	current_state = "start"
+
+# Update Dialogue branch and state after selection
+func set_dialogue_state(state):
+	current_state = state
+	pass
