cea7efd13a7d12fb320e6a770b8d698703d04da4
Yara Studer <yarastuder@gmail.com>
2025-04-22 15:09:56 -0400

    Added patience bar coloring

--- Entities/Customer/customer.gd
+++ Entities/Customer/customer.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var player_ice_cream = $"../../../Player/IceCream"
 @onready var patience_timer: Timer = $PatienceTimer
 @onready var patience_bar: ProgressBar = $ProgressBar
+var patience_time = 10
 
 func _ready():
 	interactable.interact = _on_interact
@@ -15,10 +16,16 @@ func _ready():
 	for i in range(num_of_scoops):
 		var flavor = (randi() % 3) + 1
 		ice_cream.add_scoop(flavor)
-	patience_timer.start()
+	patience_timer.start(patience_time)
 	
 func _process(delta: float) -> void:
 	patience_bar.value = patience_timer.time_left
+	if(patience_bar.value < patience_time/3):
+		patience_bar.get("theme_override_styles/fill").bg_color = Color.DARK_RED
+	elif(patience_bar.value < (patience_time/3) * 2):
+		patience_bar.get("theme_override_styles/fill").bg_color = Color.GOLDENROD
+	else:
+		patience_bar.get("theme_override_styles/fill").bg_color = Color.DARK_GREEN
 	
 func _on_interact():
 	if player_ice_cream.hasCone && player_ice_cream.scoops == ice_cream.scoops:
