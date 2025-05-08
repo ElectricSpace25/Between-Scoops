026224f839dfe7cec82502188b65b4bbc0c6838c
Yara Studer <yarastuder@gmail.com>
2025-05-08 16:16:30 -0400

    Added roommate appearing in bedroom

--- Levels/Bedroom/bedroom.gd
+++ Levels/Bedroom/bedroom.gd
@@ -2,12 +2,18 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $Player
 @onready var audio_manager: Node2D = $BedroomAudioManager
+@onready var door: Sprite2D = $Door
+@onready var roommate_1: CharacterBody2D = $Roommate1
 
 func _ready() -> void:
 	audio_manager.play_background()
 	if GlobalVariables.day == 1:
 		player.can_move = false
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(1.5).timeout
 		audio_manager.play_knock()
+		await get_tree().create_timer(2.0).timeout
+		door.frame = 1
+		roommate_1.visible = true
+		player.can_move = true
 		
 # REMEMBER TO INCREMENT DAY AT THE END
